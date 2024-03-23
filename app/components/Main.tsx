e15f6a7d393fa51f5eb52929584580a1aefd6cb0
Cameron Lee <cal26@sfu.ca>
2024-03-23 11:36:09 -0700

    basic main page layout

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
 import Map from "@/app/components/Map";
 
 const Main: React.FC = () => {
-    return <div>
-        <Map></Map>
-    </div>
+	return (
+		<div className="flex flex-col align-middle text-center w-screen min-h-screen p-8">
+			<h1 className="text-2xl">Viewing SFU From A New Perspective</h1>
+			<p className="mt-4">Click on a pin to see a camera view of SFU</p>
+			<div className="flex flex-1 justify-center items-center w-full">
+				<Map></Map>
+			</div>
+		</div>
+	);
 };
 
 export default Main;
