d774dddac7969c9d97244d595946dc8591d2cedd
Ewan Brinkman <ewanbrinkman@gmail.com>
2024-03-23 13:31:42 -0700

    Make fade faster

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -13,7 +13,17 @@ const Main: React.FC = () => {
 					<Camera
 						name="gaglardi-current"
 						description="Gaglardi intersection."
-						position={[49.274625147218664, -122.92517058936349]}
+						position={[49.27382446075663, -122.92559885491221]}
+					/>
+                    <Camera
+						name="aqn-current"
+						description="AQ."
+						position={[49.27855484810059, -122.9174309249192]}
+					/>
+                    <Camera
+						name="aqsw-current"
+						description="AQ south-west view from the south-west corner."
+						position={[49.27855484810059, -122.9174309249192]}
 					/>
 				</Map>
 			</div>
