cf89909ac7ed3e0ac7df0872590a37b7ab52d6e0
Cameron Lee <cal26@sfu.ca>
2024-03-23 13:11:29 -0700

    added title card

---
+++ app/components/TitleCard.tsx
@@ -0,0 +1,11 @@
+const TitleCard: React.FC = () => {
+	return (
+		<div className="absolute m-auto left-0 right-0 top-4 z-10 w-fit p-2 bg-slate-400 bg-opacity-60 rounded-2xl pointer-events-none">
+			<h1 className="text-3xl">Viewing SFU From a New Perspective</h1>
+			<p className="mt-2">Click on a pin to see a camera view of SFU!</p>
+			<p>(Click this message to close it)</p>
+		</div>
+	);
+};
+
+export default TitleCard;
