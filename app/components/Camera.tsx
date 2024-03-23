4443633b62a6e29f189bdebc4c6a9c763b88f10b
Ewan Brinkman <ewanbrinkman@gmail.com>
2024-03-23 12:45:05 -0700

    Make webcame image have good size

--- app/components/Camera.tsx
+++ app/components/Camera.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import CameraProps from "@typings/CameraProps";
 import { Marker, Popup } from "react-leaflet";
 
+// async function reloadImg(url: string): Promise<void> {
+// 	await fetch(url, { cache: "reload", mode: "no-cors" });
+// 	document.body
+// 		.querySelectorAll(`img[src='${url}']`)
+// 		.forEach((img) => (img.src = url));
+// }
+
 const Camera: React.FC<CameraProps> = (props: CameraProps) => {
 	return (
 		<Marker position={props.position}>
-			<Popup>
+			<Popup minWidth={750}>
 				<img
 					src={`https://ns-webcams.its.sfu.ca/public/images/${props.name}.jpg`}
 					alt={`${props.description}`}
-					style={{
-						width: 500,
-						height: 500,
-					}}
 				/>
 			</Popup>
 		</Marker>
