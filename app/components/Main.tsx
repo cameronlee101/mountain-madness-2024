880e68fb5fce96f382bfd8428db743aaaf19a860
Cameron Lee <cal26@sfu.ca>
2024-03-23 18:11:34 -0700

    began working on weather modal

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import Map from "@components/Map";
 import Camera from "@components/Camera";
 import TitleCard from "./TitleCard";
+import { WeatherModal } from "./WeatherModal";
 
 const Main: React.FC = () => {
 	return (
 		<div className="flex flex-col align-middle text-center w-screen h-screen">
 			<TitleCard />
+			<WeatherModal />
 			<div className="flex flex-1 justify-center items-center w-full">
 				<Map className="w-full h-full z-0">
 					<Camera
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mountain Madness 2024",
-  description: "Mountain Madness 2024 Project",
+	title: "Mountain Madness 2024",
+	description: "Mountain Madness 2024 Project",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Providers>{children}</Providers>
+			</body>
+		</html>
+	);
 }
